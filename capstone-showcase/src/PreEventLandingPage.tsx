ab8dd1bb4d02d6575f3adc4a0022e5fe171d7b22
Naveen Ramesh <naveenramesh987@gmail.com>
2024-06-29 18:13:06 -0700

    Temporarily added Fall 2023 paragraphs to the landing page

--- capstone-showcase/src/PreEventLandingPage.tsx
+++ capstone-showcase/src/PreEventLandingPage.tsx
@@ -5,7 +5,42 @@ const PreEventLandingPage = () => {
   return (
     <div>
       <h1>ASU Capstone Showcase</h1>
-      <p>INSERT: Location, date, time, map, summary of capstone showcase, how to navigate the website, etc</p>
+      <p className="reminder">
+        INSERT: Location, date, time, map
+        <br />
+        MODIFY: the paragraphs below, copied from the Fall 2023 capstone
+        website.
+      </p>
+      <p>
+        For the last two semesters students majoring in Computer Science,
+        Biomedical Engineering, Industrial Engineering, Electrical Engineering,
+        Computer Systems Engineering, Mechanical Engineering, Engineering
+        Management and Informatics have been working hard on industry and
+        faculty sponsored projects to solve real-world problems. Teams work
+        collaboratively to carry ideas from initial design through prototype
+        fabrication, testing and evaluation to validate their solutions. As
+        Changemakers, our seniors also consider issues related to ethics,
+        economics, safety and professional practice in their design solutions.
+      </p>
+      <p>
+        The capstone projects are designed to go beyond the classroom to bridge
+        the gap between industry and education, and is the final educational
+        element that ties to together all the engineering, science, and
+        humanities learnings as our students transition to the next phase of
+        their lives. This event is an opportunity for our students, faculty,
+        sponsors, alumni, and guests to come together to celebrate the
+        completion of the projects. We invite you to meet our students and talk
+        to them about their projects, their teamwork, and their problem-solving
+        strategies.
+      </p>
+      <p>
+        The projects are organized by major and you can navigate through the
+        site via the sidebar. If the sidebar isn't displayed, you can open it by
+        selecting the hamburger menu button (3 horizontal stacked lines) in the
+        upper left corner of this page. There is also a search tool located in
+        the upper right hand corner of the page that can help you locate a
+        specific project or team.
+      </p>
     </div>
   );
 };
