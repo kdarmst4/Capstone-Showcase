6d33ee28b8da3af5aaf651e879ef6c750b841e54
Naveen Ramesh <naveenramesh987@gmail.com>
2024-06-30 16:51:58 -0700

    Before merging

--- capstone-showcase/src/App.tsx
+++ capstone-showcase/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import PreEventLandingPage from "./PreEventLandingPage";
+import Menu from "./Menu";
 
 const App = () => {
   return (
     <div>
       <PreEventLandingPage />
+      <Menu />
     </div>
   );
 };
