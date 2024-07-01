8b3785211a2281df70c7c5d828ace4a28854da4c
Waleed Briouig <whalemen21@gmail.com>
2024-06-30 20:57:54 -0400

    Added New Pages for Different Majors and Formatted all Files

---
+++ capstone-showcase/src/Pages/ComputerSciencePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ComputerSciencePage.css'
+
+const ComputerSciencePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Computer Science</h1>
+      <p>Content for Computer Science page...</p>
+    </div>
+  );
+};
+
+export default ComputerSciencePage;
---
+++ capstone-showcase/src/Pages/ComputerSystemsEngineeringPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ComputerSystemsEngineeringPage.css';
+
+const ComputerSystemsEngineering: React.FC = () => {
+  return (
+    <div className="computer-systems-engineering-page">
+      <h1>Computer Systems Engineering</h1>
+      <p>Content for Computer Systems Engineering page...</p>
+    </div>
+  );
+};
+
+export default ComputerSystemsEngineering;
