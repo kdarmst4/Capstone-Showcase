2846ced1f66bc25a1a89e8418d2091f7a43644e5
Naveen Ramesh <naveenramesh987@gmail.com>
2024-07-07 12:55:24 -0700

    Restored App.css

--- capstone-showcase/src/App.tsx
+++ capstone-showcase/src/App.tsx
@@ -1,7 +1,7 @@
 // src/App.tsx
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./CSS/App.css";
+import "./App.css";
 import PreEventLandingPage from "./Pages/PreEventLandingPage";
 import Menu from "./Menu";
 import ComputerScience from "./Pages/ComputerScience";
