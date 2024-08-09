6b69e713e240640fddeb11625111e89bfdf1218f
Naveen Ramesh <naveenramesh987@gmail.com>
2024-08-09 15:54:47 -0700

    Less space between error messages and questions/answers

--- capstone-showcase/backend/server.js
+++ capstone-showcase/backend/server.js
@@ -8,25 +8,25 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const db = mysql.createConnection({
-    host: '162.241.218.157', //Replace with BlueHost database host
-    user: 'jmtlqnmy_showcase_entries_2024',
-    password: 'showcase2024summer', //Replace with BlueHost database password
-    database: 'jntlqnmy_capstone_project_submission', // Replace with BlueHost database name
-    authSwitchHandler: function({ pluginName, pluginData }, cb) {
-        if (pluginName === 'caching_sha2_password') {
-            const password = 'showcase2024summer'; // Replace with BlueHost MySQL root password
-            const securePassword = Buffer.from(password + '\0');
-            cb(null, securePassword);
-        }
+  host: "162.241.218.157", //Replace with BlueHost database host
+  user: "jmtlqnmy_showcase_entries_2024",
+  password: "showcase2024summer", //Replace with BlueHost database password
+  database: "jntlqnmy_capstone_project_submission", // Replace with BlueHost database name
+  authSwitchHandler: function ({ pluginName, pluginData }, cb) {
+    if (pluginName === "caching_sha2_password") {
+      const password = "showcase2024summer"; // Replace with BlueHost MySQL root password
+      const securePassword = Buffer.from(password + "\0");
+      cb(null, securePassword);
     }
+  },
 });
 
-db.connect(err => {
-    if (err) {
-        console.error('Error connecting to MySQL:', err);
-        process.exit(1);
-    }
-    console.log('MySQL Connected...');
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL:", err);
+    process.exit(1);
+  }
+  console.log("MySQL Connected...");
 });
 
 app.post("/api/survey", (req, res) => {
@@ -46,15 +46,31 @@ app.post("/api/survey", (req, res) => {
   console.log("Received survey data:", req.body);
   const sql =
     "INSERT INTO survey_entries (email, name, projectTitle, description, sponsor, teamMembers, courseNumber, demo, power, nda, youtubeLink) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-  console.log('Executing SQL:', sql);
-  db.query(sql, [email, name, projectTitle, projectDescription, sponsor, teamMembers, courseNumber, demo, power, nda, youtubeLink], (err, result) => {
+  console.log("Executing SQL:", sql);
+  db.query(
+    sql,
+    [
+      email,
+      name,
+      projectTitle,
+      projectDescription,
+      sponsor,
+      teamMembers,
+      courseNumber,
+      demo,
+      power,
+      nda,
+      youtubeLink,
+    ],
+    (err, result) => {
       if (err) {
-          console.error('Error inserting survey data:', err);
-          return res.status(500).send('Server error');
+        console.error("Error inserting survey data:", err);
+        return res.status(500).send("Server error");
       }
-      console.log('Survey data inserted successfully');
-      res.status(200).send('Survey data inserted');
-  });
+      console.log("Survey data inserted successfully");
+      res.status(200).send("Survey data inserted");
+    }
+  );
 });
 
 app.get("/api/survey/:major", (req, res) => {
