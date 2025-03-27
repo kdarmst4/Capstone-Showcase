07f0f28791ecfcb804a69b7e8ce844f2377abd94
Karina528 <91285744+Karina528@users.noreply.github.com>
2025-03-26 18:48:41 -0700

    Updated ComputerScience.tsx

--- capstone-showcase/src/Pages/ComputerScience.tsx
+++ capstone-showcase/src/Pages/ComputerScience.tsx
@@ -1,29 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { useMenuContext } from "../MenuContext";
 import "../CSS/ComputerScience.css";
-// import { capstoneDescription } from "../TextContent";
 import asuLogo from "../assets/asuLogo.png";
-import Footer from './Footer';
 import { useNavigate } from "react-router-dom";
+import { FaClipboard, FaUsers } from "react-icons/fa";
+import Footer from "./Footer";
+import { AuthContext } from "../Pages/AuthContext";
 
 const ComputerScience: React.FC = () => {
   const { isSideMenu } = useMenuContext();
   const navigate = useNavigate();
-  const [projects, setProjects] = useState<any[]>([]); // State to store fetched projects
+  const [projects, setProjects] = useState<any[]>([]);
+  const [selectedProject, setSelectedProject] = useState<any | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { isAuthenticated, userRole } = useContext(AuthContext);
 
   useEffect(() => {
     document.body.classList.add("computer-science-page-body");
 
-    // Fetch projects for the Computer Science major
-    fetch("https://asucapstone.com:3000/api/survey/computer-science")
-    //fetch("http://localhost:3000/api/survey/computer-science")
+    fetch("http://localhost:3000/api/survey/computer-science")
       .then((response) => {
         if (!response.ok) {
           throw new Error(`Error: ${response.statusText}`);
         }
         return response.json();
       })
-      .then((data) => setProjects(data)) // Populate the state with fetched projects
+      .then((data) => setProjects(data))
       .catch((error) => console.error("Error fetching projects:", error));
 
     return () => {
@@ -41,6 +43,24 @@ const ComputerScience: React.FC = () => {
     navigate("/survey");
   };
 
+  const handleFeedbackFormClick = () => {
+    navigate("/survey");
+  };
+
+  const handleMoreProjectsClick = () => {
+    navigate("/interdisciplinary");
+  };
+
+  const handleProjectClick = (project: any) => {
+    setSelectedProject(project);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedProject(null);
+  };
+
   return (
     <div className={`computer-science ${isSideMenu ? "compressed" : ""}`}>
       <header className="header-background"></header>
@@ -50,18 +70,28 @@ const ComputerScience: React.FC = () => {
             <img src={asuLogo} alt="ASU Logo" className="asu-logo" />
             <div className="title-container">
               <h3>Computer Science</h3>
-              <button
-                className="survey-form-button"
-                onClick={handleSurveyFormClick}
-                aria-label="Survey Form Button"
-              >
-                Survey Form
-              </button>
+              {isAuthenticated && userRole === "Student" && (
+                <button
+                  className="survey-form-button"
+                  onClick={handleSurveyFormClick}
+                  aria-label="Survey Form Button"
+                >
+                  Survey Form
+                </button>
+              )}
+              {isAuthenticated && userRole === "Sponsor" && (
+                <button
+                  className="feedback-form-button"
+                  onClick={handleFeedbackFormClick}
+                  aria-label="Feedback Form Button"
+                >
+                  Sponsor Feedback Form
+                </button>
+              )}
             </div>
           </article>
         </section>
 
-        {/* Render the list of projects */}
         <section className="projects-list">
           {projects.length === 0 ? (
             <p>No projects available for Computer Science.</p>
@@ -69,17 +99,11 @@ const ComputerScience: React.FC = () => {
             projects.map((project, index) => (
               <div
                 key={project.id}
-                className={`project-card ${
-                  index % 2 === 0 ? "zigzag-left" : "zigzag-right"
-                }`}
+                className={`project-card ${index % 2 === 0 ? "zigzag-left" : "zigzag-right"}`}
+                onClick={() => handleProjectClick(project)}
               >
                 {index % 2 === 0 && project.youtubeLink && (
-                  <a
-                    href={project.youtubeLink}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    aria-label="YouTube Video Link"
-                  >
+                  <a href={project.youtubeLink} target="_blank" rel="noopener noreferrer">
                     <img
                       src={extractYouTubeThumbnail(project.youtubeLink) || ""}
                       alt={`${project.projectTitle} Thumbnail`}
@@ -89,16 +113,12 @@ const ComputerScience: React.FC = () => {
                 )}
                 <div className="project-details">
                   <h4 className="project-title">{project.projectTitle}</h4>
-                  <p className="project-description">{project.projectDescription}</p>
-                  <p><strong>Team Members:</strong> {project.teamMemberNames}</p>
+                  <p>
+                    <FaUsers style={{ marginRight: "10px" }} /> {project.teamMemberNames}
+                  </p>
                 </div>
                 {index % 2 !== 0 && project.youtubeLink && (
-                  <a
-                    href={project.youtubeLink}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    aria-label="YouTube Video Link"
-                  >
+                  <a href={project.youtubeLink} target="_blank" rel="noopener noreferrer">
                     <img
                       src={extractYouTubeThumbnail(project.youtubeLink) || ""}
                       alt={`${project.projectTitle} Thumbnail`}
@@ -109,8 +129,78 @@ const ComputerScience: React.FC = () => {
               </div>
             ))
           )}
+                              <button
+                  className="more-projects-button"
+                  onClick={handleMoreProjectsClick}
+                  aria-label="More Projects Button"
+                >
+                  Like what you see? Click here to see interdisciplinary projects!
+                </button>
         </section>
       </main>
+
+      {isModalOpen && selectedProject && (
+        <div className="modal-overlay" onClick={closeModal}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <button className="close-modal" onClick={closeModal}>X</button>
+            <div className="project-menu">
+              <header className="modal-header-background" >
+              </header>
+            </div>           
+            <div className="project-header">
+              <div className="project-image">
+                {selectedProject.youtubeLink && (
+                  <a 
+                    href={selectedProject.youtubeLink} 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                    aria-label="Project Video"
+                  >
+                    <img 
+                      src={extractYouTubeThumbnail(selectedProject.youtubeLink) || ""} 
+                      alt={`${selectedProject.projectTitle} Thumbnail`}
+                    />
+                  </a>
+                )}
+              </div>
+
+              <div className="project-info">
+                <span className="semester-tag">Fall 2024</span>
+                <img src={asuLogo} alt="ASU Logo" className="modal-asu-logo" />
+                <h2 className="project-title">
+                  {selectedProject.projectTitle}
+                </h2>
+                <p className="project-category">
+                <FaClipboard style={{ marginRight: '8px' }} /> 
+                Computer Science</p>
+
+                <p className="team-members">
+                  <FaUsers style={{ marginRight: '8px' }} />
+                  {selectedProject.teamMemberNames}
+                </p>
+              </div>
+            </div>
+            <div className="project-details">
+              <div className="left-section">
+                <h3>Team Mentors</h3>
+                <p>TBA</p>
+              <div className="right-section">
+                <div className="poster-container">
+                  <p>
+                    <i className="fas fa-file-pdf poster-icon"></i> <strong>Poster</strong>
+                  </p>
+                  <a href={selectedProject.posterLink} target="_blank" rel="noopener noreferrer">
+                    <button className="poster-button">View the poster</button>
+                  </a>
+                </div>
+              </div>
+                <h3>Abstract</h3>
+                <p>{selectedProject.projectDescription}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
       <Footer />
     </div>
   );
