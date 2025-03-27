9f2486ce102c76d601213a97996b58cac3d468a1
Karina528 <91285744+Karina528@users.noreply.github.com>
2025-03-27 14:36:57 -0700

    Update App.tsx

--- capstone-showcase/src/App.tsx
+++ capstone-showcase/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { MenuProvider } from "./MenuContext";
+import { AuthProvider } from "./Pages/AuthContext";
 import Menu from "./Menu";
 import PreEventLandingPage from "./Pages/PreEventLandingPage";
 import ComputerScience from "./Pages/ComputerScience";
@@ -19,53 +20,45 @@ import AdminDashboard from "./Pages/AdminDashboard";
 import EditPresentation from "./Pages/EditPresentation";
 import EditSubmissions from "./Pages/EditSubmissions";
 import ContactSupport from "./Pages/ContactSupport";
+import StudentLogin from "./Pages/StudentLogin";
+import SponsorLogin from "./Pages/SponsorLogin";
 
 const App: React.FC = () => {
   return (
-    <MenuProvider>
-      <Router>
-        <Menu />
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<PreEventLandingPage />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/computer-science" element={<ComputerScience />} />
-            <Route
-              path="/computer-systems-engineering"
-              element={<ComputerSystemsEngineering />}
-            />
-            <Route
-              path="/biomedical-engineering"
-              element={<BiomedicalEngineering />}
-            />
-            <Route
-              path="/mechanical-engineering"
-              element={<MechanicalEngineering />}
-            />
-            <Route
-              path="/electrical-engineering"
-              element={<ElectricalEngineering />}
-            />
-            <Route
-              path="/industrial-engineering"
-              element={<IndustrialEngineering />}
-            />
-            <Route path="/informatics" element={<Informatics />} />
-            <Route path="/interdisciplinary" element={<Interdisciplinary />} />
-            <Route path="/survey" element={<Survey />} />
-            <Route path="/admin" element={<AdminLogin />} />
-            <Route path="admin-dashboard" element={<AdminDashboard pageTitle="Dashboard" />} />
-            <Route path="admin-dashboard/edit" element={<AdminDashboard pageTitle="Edit" />} />
-            <Route path="admin-dashboard/edit/presentation" element={<EditPresentation />} />
-            <Route path="admin-dashboard/edit/submissions" element={<EditSubmissions />} />
-
-
-            <Route path="admin-dashboard/support" element={<ContactSupport />} />
-          </Routes>
-        </div>
-      </Router>
-    </MenuProvider>
+    <AuthProvider>
+      <MenuProvider>
+        <Router>
+          <Menu />
+          <div className="content">
+            <Routes>
+              <Route path="/" element={<PreEventLandingPage />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/computer-science" element={<ComputerScience />} />
+              <Route path="/computer-systems-engineering" element={<ComputerSystemsEngineering />} />
+              <Route path="/biomedical-engineering" element={<BiomedicalEngineering />} />
+              <Route path="/mechanical-engineering" element={<MechanicalEngineering />} />
+              <Route path="/electrical-engineering" element={<ElectricalEngineering />} />
+              <Route path="/industrial-engineering" element={<IndustrialEngineering />} />
+              <Route path="/informatics" element={<Informatics />} />
+              <Route path="/interdisciplinary" element={<Interdisciplinary />} />
+              <Route path="/survey" element={<Survey />} />
+              <Route path="/admin" element={<AdminLogin />} />
+              <Route path="/admin-dashboard" element={<AdminDashboard pageTitle="Dashboard" />} />
+              <Route path="/admin-dashboard/edit" element={<AdminDashboard pageTitle="Edit" />} />
+              <Route path="/admin-dashboard/edit/presentation" element={<EditPresentation />} />
+              <Route path="/admin-dashboard/edit/submissions" element={<EditSubmissions />} />
+              <Route path="/student" element={<StudentLogin />} />
+              <Route path="/sponsor" element={<SponsorLogin />} />
+              <Route path="/admin-dashboard/support" element={<ContactSupport />} />
+            </Routes>
+          </div>
+        </Router>
+      </MenuProvider>
+    </AuthProvider>
   );
+};
+
+export default App;
 };
 
 export default App;
