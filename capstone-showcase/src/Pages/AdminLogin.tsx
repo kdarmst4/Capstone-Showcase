fa415f74e755c03bcdaeaf0540ed80195d0854b6
Achu Worifung <achuworifung@gmail.com>
2025-09-14 20:53:11 -0700

    revamped admin dashbard --deleted old code --changed navbar --empleted component logout logic (doesnt log out yet)

--- capstone-showcase/src/Pages/AdminLogin.tsx
+++ capstone-showcase/src/Pages/AdminLogin.tsx
@@ -3,19 +3,14 @@ import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../AuthContext";
 import "../CSS/AdminLogin.css";
 import asuLogoPlain from "../assets/asuLogoPlain.png";
+import { EyeOff, Eye } from "lucide-react";
 
 const AdminLogin: React.FC = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
   const { setAuth } = useContext(AuthContext);
-
-  // useEffect(() => {
-  //   document.body.classList.add("admin-login-page-body");
-  //   return () => {
-  //     document.body.classList.remove("admin-login-page-body");
-  //   };
-  // }, []);
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
@@ -32,24 +27,37 @@ const AdminLogin: React.FC = () => {
     <div className="container">
       <div className="box">
         <img src={asuLogoPlain} alt="ASU Logo" className="logo" />
+        <h1>ASU Capstone Showcase</h1>
         <h2 className="admin-login-title">Admin Login</h2>
-        <form onSubmit={handleSubmit}>
-          <input
+        <form onSubmit={handleSubmit} className="admin-form">
+          <span className="input">
+            <input
             type="text"
             placeholder="Username"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
             required
-            className="input"
+            className="admin-input"
           />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            className="input"
-          />
+          </span>
+
+          <span className="input">
+            <input
+              type={showPassword ? "text" : "password"}
+              
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="admin-input"
+            />
+            {showPassword ? (
+              <Eye onClick={() => setShowPassword(false)} className="eye-icon" />
+            ) : (
+              <EyeOff onClick={() => setShowPassword(true)} className="eye-icon" />
+            )}
+          </span>
+
           <button type="submit" className="button">
             Login
           </button>
