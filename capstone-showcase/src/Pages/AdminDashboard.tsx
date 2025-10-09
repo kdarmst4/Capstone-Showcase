abe57690a36c578d90c457b1f94380660a7dabb7
worifung achu <achuclinton47@gmail.com>
2025-10-09 12:32:50 -0700

    improved admin navbar switch to mobile view --navbar no longer has diff color in pc view change chortcut bg color added loading component when validating credentials

--- capstone-showcase/src/Pages/AdminDashboard.tsx
+++ capstone-showcase/src/Pages/AdminDashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState , useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "../CSS/AdminDashboard.css";
 import { Winners } from "../AdminWinners";
@@ -44,8 +44,8 @@ const AdminDashboard: React.FC<AdminDashboardProps> = () => {
   const [loggingOut, setLoggingOut] = useState(false);
   const [pageTitle, setPageTitle] = useState("Dashboard");
   const [showMenu, setShowMenu] = useState(false);
-  const {  isSignedIn, isTokenValid, setIsSignedIn, setToken } = useAuth();
-
+  const [loading, setLoading] = useState(true);
+  const { isSignedIn, isTokenValid, setIsSignedIn, setToken } = useAuth();
 
   const handleLogout = () => {
     setIsSignedIn(false);
@@ -56,110 +56,120 @@ const AdminDashboard: React.FC<AdminDashboardProps> = () => {
   useEffect(() => {
     if (!isSignedIn) {
       navigate("/admin");
-    }
-    else if (!isTokenValid())
-    {
+    } else if (!isTokenValid()) {
       setIsSignedIn(false);
       setToken(null);
       navigate("/admin");
     }
+
+    setLoading(false);
   }, [isSignedIn, navigate]);
-
-
-
-
-
-
 
   const changeTitle = (title: string) => {
     setPageTitle(title);
-  }
+  };
 
   return (
-    <div className="admin-dashboard-container">
-      {loggingOut && (
-        <div
-          className="admin-logout-shade"
-          onClick={() => setLoggingOut(false)}
-        >
-          <div className="admin-logout-msg">
-            <LogOut size={50} className="admin-logout-icon" />
-            <h2>Log Out</h2>
-            <p>
-              Are you sure you want to log out? You’ll need to sign in again to
-              access your dashboard.
-            </p>
-            <button className="admin-logout-yes" onClick={handleLogout}>
-              Yes, Log Me Out
-            </button>
-            <button
-              className="admin-logout-no"
-              onClick={() => setLoggingOut(false)}
-            >
-              Cancel
-            </button>
-          </div>
+    <>
+      {loading && (
+        <div className="loading-shade">
+          <div className="loading-spinner"></div>
         </div>
       )}
-
-      {/* --- ADDED FOR BETTER MOBILE UX --- */}
-      {showMenu && <div className="mobile-overlay" onClick={() => setShowMenu(false)}></div>}
-
-      <div className="admin-dashboard-header ">
-        <span className="title">{pageTitle}</span>
-        <span className="menu" onClick={() => setShowMenu(!showMenu)}>
-          <Menu />
-        </span>
-      </div>
-      <div className="admin-dashboard-body">
-        <nav className={`admin-dashboard-navbar ${showMenu ? 'admin-nav-show' : ''}`}>
-          <div className="admin-dashboard-navbar-options">
-            <ul className="admin-dashboard-navbar-list">
-              {sidebarOptions.map((option) => (
-                <li
-                  key={option.label}
-                  className="admin-dashboard-navbar-items"
-                  onClick={() => {
-                    setPageTitle(option.label);
-                    setShowMenu(false); 
-                  }}
-                >
-                  {option.icon}
-                  {option.label}
-                </li>
-              ))}
-              <a
-                href="https://betasubmission.asucapstone.com/login"
-                className="admin-dashboard-navbar-items"
+      <div className="admin-dashboard-container">
+        {loggingOut && (
+          <div
+            className="admin-logout-shade"
+            onClick={() => setLoggingOut(false)}
+          >
+            <div className="admin-logout-msg">
+              <LogOut size={50} className="admin-logout-icon" />
+              <h2>Log Out</h2>
+              <p>
+                Are you sure you want to log out? You’ll need to sign in again
+                to access your dashboard.
+              </p>
+              <button className="admin-logout-yes" onClick={handleLogout}>
+                Yes, Log Me Out
+              </button>
+              <button
+                className="admin-logout-no"
+                onClick={() => setLoggingOut(false)}
               >
-                <PackageMinus />
-                Go to Sponsor Page
-              </a>
-            </ul>
+                Cancel
+              </button>
+            </div>
           </div>
+        )}
 
-          <div className="admin-dashboard-logout-container">
-            <button
-              className="admin-dashboard-logout-button"
-              onClick={() => {
-                setLoggingOut(true);
-              }}
-            >
-              <LogOut size={16} />
-              <span>Logout</span>
-            </button>
-          </div>
-        </nav>
-        <main className="admin-dashboard-main">
-          {pageTitle === "Dashboard" && (
-            <AdminDashboardShortcut changeTitle={setPageTitle} />
-          )}
-          {pageTitle === "Download Database" && <DownloadProjects />}
-          {pageTitle === "Make Edits" && <Edit />}
-          {pageTitle === "Winners" && <Winners />}
-        </main>
+        {/* --- ADDED FOR BETTER MOBILE UX --- */}
+        {showMenu && (
+          <div
+            className="mobile-overlay"
+            onClick={() => setShowMenu(false)}
+          ></div>
+        )}
+
+        <div className="admin-dashboard-header ">
+          <span className="title">{pageTitle}</span>
+          <span className="menu" onClick={() => setShowMenu(!showMenu)}>
+            <Menu />
+          </span>
+        </div>
+        <div className="admin-dashboard-body">
+          <nav
+            className={`admin-dashboard-navbar ${
+              showMenu ? "admin-nav-show" : ""
+            }`}
+          >
+            <div className="admin-dashboard-navbar-options">
+              <ul className="admin-dashboard-navbar-list">
+                {sidebarOptions.map((option) => (
+                  <li
+                    key={option.label}
+                    className="admin-dashboard-navbar-items"
+                    onClick={() => {
+                      setPageTitle(option.label);
+                      setShowMenu(false);
+                    }}
+                  >
+                    {option.icon}
+                    {option.label}
+                  </li>
+                ))}
+                <a
+                  href="https://betasubmission.asucapstone.com/login"
+                  className="admin-dashboard-navbar-items"
+                >
+                  <PackageMinus />
+                  Go to Sponsor Page
+                </a>
+              </ul>
+            </div>
+
+            <div className="admin-dashboard-logout-container">
+              <button
+                className="admin-dashboard-logout-button"
+                onClick={() => {
+                  setLoggingOut(true);
+                }}
+              >
+                <LogOut size={16} />
+                <span>Logout</span>
+              </button>
+            </div>
+          </nav>
+          <main className="admin-dashboard-main">
+            {pageTitle === "Dashboard" && (
+              <AdminDashboardShortcut changeTitle={setPageTitle} />
+            )}
+            {pageTitle === "Download Database" && <DownloadProjects />}
+            {pageTitle === "Make Edits" && <Edit />}
+            {pageTitle === "Winners" && <Winners />}
+          </main>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
