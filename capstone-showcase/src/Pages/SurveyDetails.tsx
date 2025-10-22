12e439b8daabe54dd692ee059a4f1eb5dd040337
Achu Worifung <achuworifung@gmail.com>
2025-10-21 17:38:54 -0700

    added poster and team img to survey details

--- capstone-showcase/src/Pages/SurveyDetails.tsx
+++ capstone-showcase/src/Pages/SurveyDetails.tsx
@@ -221,7 +221,7 @@ export function SurveyDetails() {
                       }}
                     />
                   </span>
-                  <p>{name}</p>
+                  {/* <p>{name}</p> */}
                 </span>
               ))}
             </div>
