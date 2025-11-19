5ad651492ca05338ab98e954bc3dd3cc05708916
Joshua Decker <35786437+Code-Level-Beard@users.noreply.github.com>
2025-11-19 14:26:47 -0700

    Update DownloadProjects.tsx

--- capstone-showcase/src/DownloadProjects.tsx
+++ capstone-showcase/src/DownloadProjects.tsx
@@ -121,7 +121,7 @@ export function DownloadProjects() {
     document.body.removeChild(link);
   };
   const API_BASE_URL =
-    process.env.NODE_ENV === "production"
+    import.meta.env.PROD
       ? "/api"
       : "http://localhost:3000/api";
   const csvHeaders = [
